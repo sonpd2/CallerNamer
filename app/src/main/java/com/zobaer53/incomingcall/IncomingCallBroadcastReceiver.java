7f25518e5cbf2cee9040a06f23081dc104f9b547
Duong Duc <ducdv28@gmail.com>
2023-09-21 11:26:11 +0700

    ducdv

--- app/src/main/java/com/zobaer53/incomingcall/IncomingCallBroadcastReceiver.java
+++ app/src/main/java/com/zobaer53/incomingcall/IncomingCallBroadcastReceiver.java
@@ -12,7 +12,6 @@ import android.os.Build;
 import android.provider.ContactsContract;
 import android.telephony.TelephonyManager;
 import android.util.DisplayMetrics;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
@@ -21,38 +20,16 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
-import android.os.AsyncTask;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.IOException;
-import okhttp3.MediaType;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
-
 public class IncomingCallBroadcastReceiver extends BroadcastReceiver {
     private static WindowManager windowManager;
     @SuppressLint("StaticFieldLeak")
     private static ViewGroup windowLayout;
 
     private static final float WINDOW_WIDTH_RATIO = 0.8f;
-    private static final String TAG = "ducdv";
     private WindowManager.LayoutParams params;
     private float x;
     private float y;
     private String name;
-    private String position;
-    private String company;
-
-    // Replace with your API endpoint and API key
-    String apiUrl = "https://vops.viettel.vn/api/v1/contacts.search-by-org";
-    String apiKey = "Z8BXBjCoFMbppDNLkdjdVidTSAQwpV2h-Ff_EpNC1f-";
-    String apiUser = "jE5TLGK33qi7GR3nW";
-
 
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -72,83 +49,10 @@ public class IncomingCallBroadcastReceiver extends BroadcastReceiver {
         }
     }
 
-    private class ApiPostRequestTask extends AsyncTask<String, Void, String> {
-
-        @Override
-        protected String doInBackground(String... params) {
-            String apiUrl = params[0];
-            String jsonData = params[1];
-            String apiKey = params[2];
-            String apiUser = params[3];
-
-            OkHttpClient client = new OkHttpClient();
-
-            MediaType JSON = MediaType.parse("application/json");
-            RequestBody body = RequestBody.create(jsonData, JSON);
-
-            Request request = new Request.Builder()
-                    .url(apiUrl)
-                    .addHeader("X-Auth-Token", apiKey)
-                    .addHeader("X-User-Id", apiUser)
-                    .post(body)
-                    .build();
-
-            try {
-                Response response = client.newCall(request).execute();
-                if (response.isSuccessful()) {
-                    return response.body().string();
-                } else {
-                    return "Error: " + response.code() + " " + response.message();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-                return "Error: " + e.getMessage();
-            }
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            if (result != null) {
-                // Process the API response here
-                //resultTextView.setText(result);
-                Log.d(TAG, "Response: " + result);
-                parseJson(result);
-            } else {
-                //resultTextView.setText("Error fetching data from the API.");
-                Log.d(TAG, "Response: " + "Error fetching data from the API.");
-            }
-        }
-    }
-
-    private void parseJson(String response){
-        try {
-            JSONObject jsonObject = new JSONObject(response);
-            JSONArray results = jsonObject.getJSONArray("contacts");
-            for (int i = 0; i < results.length(); i++) {
-                JSONObject jo = results.getJSONObject(i);
-                name = jo.getString("name");
-                position = jo.getString("position");
-                company = jo.getString("company");
-                Log.d(TAG, "Hi: " + name  +" "+ position +" "+ company);
-                TextView nameTextView = windowLayout.findViewById(R.id.name);
-                TextView posTextView = windowLayout.findViewById(R.id.position);
-                TextView compTextView = windowLayout.findViewById(R.id.company);
-                nameTextView.setText(name);
-                posTextView.setText(position);
-                compTextView.setText(company);
-            }
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-    }
-
     private void showWindow(final Context context, String phone) {
 
-        if(phone!= null) {
-            // JSON data to send in the request
-            String jsonData = "{\"org_paths\":[],\"text\":\""+ phone +"\",\"page\":1}";
-            // Execute the AsyncTask to make the API POST request
-            new ApiPostRequestTask().execute(apiUrl, jsonData, apiKey, apiUser);
+        if(!getContactId(phone, context).isEmpty()) {
+            name = getContactName(getContactId(phone, context), context);
         }else
             name = "Unknown";
 
@@ -156,10 +60,14 @@ public class IncomingCallBroadcastReceiver extends BroadcastReceiver {
         windowLayout = (ViewGroup) View.inflate(context, R.layout.window_call_info, null);
         getLayoutParams();
         setOnTouchListener();
+
         TextView numberTextView = windowLayout.findViewById(R.id.number);
+        TextView nameTextView = windowLayout.findViewById(R.id.name);
         numberTextView.setText(phone);
+        nameTextView.setText(name);
         Button cancelButton = windowLayout.findViewById(R.id.cancel);
         cancelButton.setOnClickListener(view -> closeWindow());
+
         windowManager.addView(windowLayout, params);
     }
 
